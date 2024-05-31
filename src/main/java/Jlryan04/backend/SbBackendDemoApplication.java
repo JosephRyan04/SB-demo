f60db9670533ad9ad7e43a73788bad2d3d4359ac
Jlryan04 <lkyjosephryan@gmail.com>
2024-05-30 23:36:53 -0400

    Initial commit

---
+++ src/main/java/Jlryan04/backend/SbBackendDemoApplication.java
@@ -0,0 +1,13 @@
+package Jlryan04.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbBackendDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbBackendDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Jlryan04/backend/SbBackendDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package Jlryan04.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbBackendDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
